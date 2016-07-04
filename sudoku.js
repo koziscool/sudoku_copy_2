7926981e92a5ee807eaf91833a956e26c6b35103
koziscool <kozjob@yahoo.com>
2016-07-04 10:55:17 -0500

    stuff

--- sudoku.js
+++ sudoku.js
@@ -1,4 +1,6 @@
 
+var test_grid2 = "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......";
+
 var solve = function() {
   console.log( "solve" );
 };
@@ -11,11 +13,11 @@ var pick = function() {
 
 // this is imperative, could refactor to function later
 var outerBlock = function() {
-  var $table = $('<table></table>').addClass('outer-block');
+  var $table = $('<table>').addClass('outer-block');
   for(var i=0; i<3; i++){
-    var $row = $('<tr></tr>').addClass('outer-block-row');
+    var $row = $('<tr>').addClass('outer-block-row');
     for(var j=0; j<3; j++){
-      var $col = $('<td></td>').addClass('outer-block-data');
+      var $col = $('<td>').addClass('outer-block-data');
       $col.append( innerBlock() );
       $row.append( $col );
     }
@@ -24,26 +26,37 @@ var outerBlock = function() {
   return $table;
 }
 
-
 var innerBlock = function() {
-  var $table = $('<table></table>').addClass('inner-block');
+  var $table = $('<table>').addClass('inner-block');
   for(var i=0; i<3; i++){
-    var $row = $('<tr></tr>').addClass('inner-block-row');
+    var $row = $('<tr>').addClass('inner-block-row');
     for(var j=0; j<3; j++){
-      var $col = $('<td></td>').addClass('inner-block-data').text( (3*i +j + 1).toString() );
+      // var $col = $('<td>').addClass('inner-block-data').text( (3*i +j + 1).toString() );
+      var $col = $('<td>');
+
+      var cellData =  (3*i +j + 1).toString();
+      var inputString = '<input type="text" maxlength="1" size="1" " value="'  + cellData + '"/>;'
+      var $input = $(inputString);
+
+      $col.append($input);
       $row.append($col);
     }
     $table.append($row);
   }  
   return $table;
-}
+};
+
 
 var drawPuzzle = function() {
   console.log( "draw" );
 
   var $puzzleGrid = $('#puzzle-grid');
-  // var newStuff = "<div>koz is really cool</div>"
-// $('#here_table').append(table);
-
   $puzzleGrid.append( outerBlock() );
 };
+
+$( document ).ready(function(){
+
+  drawPuzzle();
+
+});
+
